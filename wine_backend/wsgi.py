ef4f0b2656df312c0e233b66d9c9747c35866126
lavaliani <59194082+lavaliani@users.noreply.github.com>
2025-03-11 10:26:34 +0400

    Update wsgi.py

--- wine_backend/wsgi.py
+++ wine_backend/wsgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# შეცვალე ეს ხაზის მითითება
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+# სწორი ბილიკი, რომელიც მიუთითებს 'settings.py' ფაილზე
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wine-fermentation.settings')
 
 application = get_wsgi_application()
