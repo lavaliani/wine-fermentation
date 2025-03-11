e743ab72f3b759e4a2cbcddbd2b70752c4172491
lavaliani <59194082+lavaliani@users.noreply.github.com>
2025-03-11 09:32:21 +0400

    Update urls.py

--- wine_app/urls.py
+++ wine_app/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
-from . import views
+from .views import home, register, login_view, projects, new_project, project_detail, delete_project
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("new_project/", views.new_project, name="new_project"),
-    path("projects/", views.projects, name="projects"),
-
-    # path("projects/", views.project_list, name="project_list"),
-    path("projects/<int:project_id>/", views.project_detail, name="project_detail"),
-    path("projects/<int:project_id>/delete/", views.delete_project, name="delete_project"),
-    
-    # დამატებული ბილიკები
-    path("register/", views.register, name="register"),
-    path("login/", views.login_view, name="login"),
+    path("", home, name="home"),
+    path("register/", register, name="register"),
+    path("login/", login_view, name="login"),
+    path("projects/", projects, name="projects"),
+    path("projects/new/", new_project, name="new_project"),
+    path("projects/<int:project_id>/", project_detail, name="project_detail"),
+    path("projects/<int:project_id>/delete/", delete_project, name="delete_project"),
 ]
