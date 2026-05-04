626b1084de21233564da7d983f061fb8b9aa0ec0
lavaliani <59194082+lavaliani@users.noreply.github.com>
2026-05-04 18:04:04 +0400

    Simplify WSGI configuration

--- wine_backend/wsgi.py
+++ wine_backend/wsgi.py
@@ -1,7 +1,8 @@
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-# სწორად ვუთითებთ ბილიკს settings.py-მდე
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wine_backend.settings')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wine_backend.settings")
 
 application = get_wsgi_application()
